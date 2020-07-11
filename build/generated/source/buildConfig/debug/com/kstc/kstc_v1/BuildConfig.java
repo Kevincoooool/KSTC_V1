af3c12f846efbbc99dc0a5bf6a5e573d99ebf685
Kevincoooool <33611679+Kevincoooool@users.noreply.github.com>
2020-07-11 19:40:33 +0800

    7-11 update gradle

--- build/generated/source/buildConfig/debug/com/kstc/kstc_v1/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/kstc/kstc_v1/BuildConfig.java
@@ -5,9 +5,8 @@ package com.kstc.kstc_v1;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.ano_rc.ano_rc_v1";
+  public static final String APPLICATION_ID = "com.kstc";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
 }
